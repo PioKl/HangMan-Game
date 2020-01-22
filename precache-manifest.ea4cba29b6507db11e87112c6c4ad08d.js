43ff9c9ead96161d7998d5a33168ce3c1794871c
PioKl <kosiu342@gmail.com>
2020-01-22 20:17:40 +0100

    Updates

---
+++ precache-manifest.ea4cba29b6507db11e87112c6c4ad08d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6498f21d464e57788d5",
+    "url": "/HangMan-Game/static/css/main.11ef121c.chunk.css"
+  },
+  {
+    "revision": "b6498f21d464e57788d5",
+    "url": "/HangMan-Game/static/js/main.5b682bfd.chunk.js"
+  },
+  {
+    "revision": "d2bc3b88eceac79bd01e",
+    "url": "/HangMan-Game/static/js/runtime~main.918ce6a4.js"
+  },
+  {
+    "revision": "6efcefea295667b6ca17",
+    "url": "/HangMan-Game/static/js/2.36a6cd5b.chunk.js"
+  },
+  {
+    "revision": "69cf5cc165a48abf413a3147a593c8dd",
+    "url": "/HangMan-Game/index.html"
+  }
+];
